31aa116ce593a3b9a4302846cbd647e505999017
RafiMusy1512 <116368674+RafiMusy1512@users.noreply.github.com>
2022-11-19 10:06:30 +0700

    Create Variebel.java

---
+++ Variebel.java
@@ -0,0 +1,5 @@
+String nama = "M. Rafi Musyaffa";
+		int tahun	= 2000;
+		System.out.println("Assalamu'alaikum");
+		System.out.println("Nama saya : " + Rafi);
+		System.out.println("Saya lahir di tahun : " + 2000);
