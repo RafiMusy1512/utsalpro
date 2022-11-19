1b903d36add944c35e0a55fa7bada85b4228157a
RafiMusy1512 <116368674+RafiMusy1512@users.noreply.github.com>
2022-11-19 12:03:12 +0700

    Add files via upload

---
+++ Variabel.java
@@ -0,0 +1,16 @@
+package utsalpro;
+
+public class Variabel {
+
+	public static void main(String[] args) {
+		System.out.println ("assalamualaikum");
+		String firstname = ("Nama saya :");
+		String lastname = (" Muhammad Rafi Musyaffa");
+		System.out.println(firstname + lastname);
+		
+		int tahun = 2000;
+		System.out.println ("Saya lahir pada tahun :" + tahun);
+
+	}
+
+}
